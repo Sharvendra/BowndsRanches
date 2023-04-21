0124344e32d9df1725b1d5f3b3868ff4c467f735
Sharvendra <sharvendra2003@gmail.com>
2023-04-21 09:43:33 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,69 @@
+// $('#page2').textillate({ in: { effect: 'fadeInDown' } });
+function init() {
+    gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+
+
+
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+}
+init()
+var overlay = document.querySelector("#overlay")
+var iscroll = document.querySelector("#scroll")
+
+overlay.addEventListener("mouseenter",function(){
+    iscroll.style.scale = 1
+})
+overlay.addEventListener("mouseleave", function(){
+    iscroll.style.scale = 0
+})
+overlay.addEventListener("mousemove",function(dets){
+  iscroll.style.left = `${dets.x-45}px`
+  iscroll.style.top = `${dets.y-38}px`
+})
+
+gsap.to("#page2 img",{
+    rotate: -5,
+    // top:60,
+    marginTop:0,
+    scrollTrigger:{
+        scroller:"#main",
+        trigger:"#page2 img",
+        scrub:0.3,
+        markers: true,
+        start:"top 60%",       
+    }
+
+})
+
+document.querySelector("#page3").addEventListener("mousemove",function(dets){
+  document.querySelector("#page3 #text-move").style.left = `${dets.x}px`
+  document.querySelector("#page3 #text-move").style.top = `${dets.y}px`
+})
